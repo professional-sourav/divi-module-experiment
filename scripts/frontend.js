a3f5e96849d14b505cfcfb8eea3bf20ab87b2f14
Sourav <ouravthedevguy@gmail.com>
2022-04-21 09:27:03 +0530

    Added ajax loader svg, changed fetch logic

--- scripts/frontend.js
+++ scripts/frontend.js
@@ -16,17 +16,87 @@ jQuery(function($) {
 
             e.preventDefault();
 
-            // $(this).parents('.csh_custom_woo_products > .et_pb_module_inner > .csh_custom_woo_products').load( $(this).attr('href') );
+            const placeholder = $(this).parents('.csh_custom_woo_products > .et_pb_module_inner > .csh_custom_woo_products');
 
-            let page_data = httpGet( $(this).attr('href') );
-            console.log(page_data);
+            console.log("Loading...");
 
-            const div = document.createElement('div');
-            div.innerHTML= page_data;
+            // show the loader
+            const overlay = `<div class="custom_woo_products_shop_overlay">
+            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="margin:auto;background:#fff;display:block;" width="200px" height="200px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
+            <g transform="rotate(0 50 50)">
+              <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#fe718d">
+                <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.9166666666666666s" repeatCount="indefinite"></animate>
+              </rect>
+            </g><g transform="rotate(30 50 50)">
+              <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#fe718d">
+                <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.8333333333333334s" repeatCount="indefinite"></animate>
+              </rect>
+            </g><g transform="rotate(60 50 50)">
+              <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#fe718d">
+                <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.75s" repeatCount="indefinite"></animate>
+              </rect>
+            </g><g transform="rotate(90 50 50)">
+              <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#fe718d">
+                <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.6666666666666666s" repeatCount="indefinite"></animate>
+              </rect>
+            </g><g transform="rotate(120 50 50)">
+              <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#fe718d">
+                <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.5833333333333334s" repeatCount="indefinite"></animate>
+              </rect>
+            </g><g transform="rotate(150 50 50)">
+              <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#fe718d">
+                <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.5s" repeatCount="indefinite"></animate>
+              </rect>
+            </g><g transform="rotate(180 50 50)">
+              <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#fe718d">
+                <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.4166666666666667s" repeatCount="indefinite"></animate>
+              </rect>
+            </g><g transform="rotate(210 50 50)">
+              <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#fe718d">
+                <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.3333333333333333s" repeatCount="indefinite"></animate>
+              </rect>
+            </g><g transform="rotate(240 50 50)">
+              <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#fe718d">
+                <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.25s" repeatCount="indefinite"></animate>
+              </rect>
+            </g><g transform="rotate(270 50 50)">
+              <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#fe718d">
+                <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.16666666666666666s" repeatCount="indefinite"></animate>
+              </rect>
+            </g><g transform="rotate(300 50 50)">
+              <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#fe718d">
+                <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.08333333333333333s" repeatCount="indefinite"></animate>
+              </rect>
+            </g><g transform="rotate(330 50 50)">
+              <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#fe718d">
+                <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="0s" repeatCount="indefinite"></animate>
+              </rect>
+            </g>
+            </svg>
+            </div>`;
 
-            var shopBlock = div.querySelector('div.woocommerce');
+            placeholder.prepend( overlay );
+            
 
-            $(this).parents('.csh_custom_woo_products > .et_pb_module_inner > .csh_custom_woo_products').html( shopBlock );
+            // fetch products 
+            fetch($(this).attr('href')).then(function (response) {
+
+                return response.text();
+
+            }).then(function (dataFromServer) {
+
+                console.log(dataFromServer);
+
+                const div = document.createElement('div');
+                div.innerHTML= dataFromServer;
+
+                const shopBlock = div.querySelector('div.woocommerce');
+
+                placeholder.html( shopBlock );
+
+            }).catch(function (err) {                
+                console.warn('Something went wrong.', err);
+            });
         });
     }
 });
